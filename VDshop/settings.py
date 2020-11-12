a945934689b7062cc5a5fb353069da715774d067
Valentine <valentyn.kofanov@knu.ua>
2020-11-12 13:24:49 +0200

    updated db

--- VDshop/settings.py
+++ VDshop/settings.py
@@ -146,8 +146,8 @@ STATICFILES_DIRS = (
 
 
 # Heroku: Update database configuration from $DATABASE_URL.
-# db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
