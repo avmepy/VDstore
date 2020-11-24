31af1a10efb117e2f36ed2764eeaa67644296aa8
Danyil <dselivanov2000@gmail.com>
2020-11-24 16:42:15 +0200

    cleaning repo

--- store/admin.py
+++ store/admin.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
-<<<<<<< HEAD
-=======
+
 
 from .models import SmartPhone, Sale, SmartWatch, Tablet, Computer, Audio, Laptop, Cart, Comment
->>>>>>> dev
 
-from .models import SmartPhone, Sale, SmartWatch, Tablet, Computer, Audio, Laptop, Cart, Comment
+
 
 admin.site.register(SmartPhone)
 admin.site.register(SmartWatch)
@@ -15,8 +13,4 @@ admin.site.register(Audio)
 admin.site.register(Laptop)
 admin.site.register(Sale)
 admin.site.register(Cart)
-<<<<<<< HEAD
 admin.site.register(Comment)
-=======
-admin.site.register(Comment)
->>>>>>> dev
